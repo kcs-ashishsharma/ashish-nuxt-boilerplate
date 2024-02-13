1e6f526190790bce7ef7e7216ff03ed81b015cdb
Ashish Sharma <ashish.sharma@ext.inxeption.com>
2024-02-13 11:38:38 +0530

    add starter_page

---
+++ pages/starter-page/index.vue
@@ -0,0 +1,27 @@
+<template lang="">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="page-title-box d-flex flex-row justify-content-between">
+                    <PageTitle :title="'Starter Page'" />
+                    <div class="page-title-right">
+                       <Breadcrumb :pageName="'Starter Page'" />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+            </div>
+        </div>
+    </div>               
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
